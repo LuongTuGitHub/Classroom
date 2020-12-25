529850435c55ca4bc0c487b68f7964eb23b32ae9
LuongTuGitHub <luongvantu_t64@hus.edu.vn>
2020-12-25 14:03:37 +0700

    m

--- app/src/main/java/com/application/project/classroom/adapter/ViewWeekStudentAdapter.java
+++ app/src/main/java/com/application/project/classroom/adapter/ViewWeekStudentAdapter.java
@@ -2,7 +2,6 @@ package com.application.project.classroom.adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,64 +9,51 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.application.project.classroom.R;
 import com.application.project.classroom.module.AddLesson;
-import com.application.project.classroom.module.Const;
 import com.application.project.classroom.object.Lesson;
 import com.application.project.classroom.object.Week;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class WeekTeacherAdapter extends RecyclerView.Adapter<WeekTeacherAdapter.WeekTeacherHolder> {
-
+public class ViewWeekStudentAdapter extends RecyclerView.Adapter<ViewWeekStudentAdapter.ViewWeekStudentHolder> {
+    private List<Week> weeks;
     private Context context;
+    private AddLesson addLesson;
+
+    public void setAddLesson(AddLesson addLesson) {
+        this.addLesson = addLesson;
+    }
 
     public void setContext(Context context) {
         this.context = context;
     }
-
-    private List<Week> weeks;
-    private AddLesson addLesson;
-
-
-
 
     public void setWeeks(List<Week> weeks) {
         this.weeks = weeks;
         notifyDataSetChanged();
     }
 
-    public void setAddLesson(AddLesson addLesson) {
-        this.addLesson = addLesson;
-    }
-
     @NonNull
     @Override
-    public WeekTeacherHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_week_teacher, parent, false);
-        return new WeekTeacherHolder(view);
+    public ViewWeekStudentHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_week_student,parent,false);
+        return new ViewWeekStudentHolder(view);
     }
 
     @SuppressLint("SetTextI18n")
     @Override
-    public void onBindViewHolder(@NonNull WeekTeacherHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewWeekStudentHolder holder, int position) {
         LessonTitleAdapter adapter = new LessonTitleAdapter();
         holder.rv_lesson.setAdapter(adapter);
         holder.rv_lesson.setLayoutManager(new LinearLayoutManager(context, RecyclerView.VERTICAL, false));
         holder.tv_week.setText("Week " + (position + 1) + "");
         holder.tv_description.setText(weeks.get(position).getTitle());
-        holder.bt_add_lesson.setOnClickListener(v -> addLesson.addLesson(v, position));
         adapter.setLessons(weeks.get(position).getLessons());
         holder.bt_toggle_show_lesson_down.setOnClickListener(v -> {
             if(holder.rv_lesson.getVisibility()==View.GONE){
@@ -106,17 +92,16 @@ public class WeekTeacherAdapter extends RecyclerView.Adapter<WeekTeacherAdapter.
         return 0;
     }
 
-    public static class WeekTeacherHolder extends RecyclerView.ViewHolder {
+    public static class ViewWeekStudentHolder extends RecyclerView.ViewHolder{
         public TextView tv_week, tv_description;
         public RecyclerView rv_lesson;
-        public Button bt_add_lesson,bt_toggle_show_lesson_down,bt_toggle_show_lesson_up;
+        public Button bt_toggle_show_lesson_down,bt_toggle_show_lesson_up;
 
-        public WeekTeacherHolder(@NonNull View itemView) {
+        public ViewWeekStudentHolder(@NonNull View itemView) {
             super(itemView);
             tv_description = itemView.findViewById(R.id.tv_description);
             tv_week = itemView.findViewById(R.id.tv_week);
             rv_lesson = itemView.findViewById(R.id.rv_lesson);
-            bt_add_lesson = itemView.findViewById(R.id.bt_add_lesson);
             bt_toggle_show_lesson_down = itemView.findViewById(R.id.bt_toggle_show_lesson_down);
             bt_toggle_show_lesson_up = itemView.findViewById(R.id.bt_toggle_show_lesson_up);
         }
